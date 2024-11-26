72785b29e18e92e1f838104c146f8c9c3c3093bf
ThiagoCL16 <thiagocavalcantelopes16@gmail.com>
2024-11-25 21:54:30 -0300

    mais js

---
+++ js/script.js
@@ -0,0 +1,46 @@
+function mostraAlerta(){
+    alert ('Isso funciona mesmo!!')
+}
+
+function mostraAlerta1(msg){
+    alert ('boa noite '+ msg)
+}
+
+function soma(n1, n2){
+    n1=parseFloat(n1)
+    n2=parseFloat(n2)
+    soma=n1+n2;
+    return(soma);
+}
+
+function dobro() {
+    var valor1, result
+    valor1 = document.getElementById("idN");
+    result = parseFloat(valor1.value) +parseFloat(valor1.value)
+    document.getElementById("idResult").innerHTML = result
+}
+
+function calculadora(op) {
+    var valor1, value2, result
+    valor1= document.getElementById("idN11")
+    valor2= document.getElementById("idN22")
+    if(op == "+") {
+        result = parseFloat(valor1.value)+parseFloat(valor2.value)
+    }
+    document.getElementById("idResposta").innerHTML = result;
+
+    if(op == "-"){
+        result = parseFloat(valor1.value)-parseFloat(valor2.value)
+    }
+    document.getElementById("idResposta").innerHTML = result;
+
+    if(op == "*"){
+        result = parseFloat(valor1.value)*parseFloat(valor2.value)
+    }
+    document.getElementById("idResposta").innerHTML = result;
+
+    if(op == "/"){
+        result = parseFloat(valor1.value)/parseFloat(valor2.value)
+    }
+    document.getElementById("idResposta").innerHTML = result;
+}
